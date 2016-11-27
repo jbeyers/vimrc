c3738bfc56fe96055b2d5290c10a2df4b203f353
Johan Beyers <johan@juizi.com>
2016-11-27 07:00:17 +0200

    Remove matchit, as the link is broken and it's part of standard

--- setup.sh
+++ setup.sh
@@ -54,9 +54,6 @@ getgit https://github.com/tpope/vim-ragtag.git vim-ragtag
 # Repeats things in script files.
 getgit https://github.com/tpope/vim-repeat.git vim-repeat
 
-# Matchit jumps between tags in html etc. Might be part of a standard install
-getgit https://github.com/edsono/vim-matchit.git vim-matchit
-
 # On-the-fly python syntax checking
 #getgit https://github.com/kevinw/pyflakes-vim.git pyflakes-vim
 
